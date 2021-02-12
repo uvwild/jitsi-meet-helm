924e15c6e60ab97b0dbb84b70a48ce7621014c25
UV <uvwild@gmail.com>
2021-02-12 07:50:56 +0000

    more cleanup for port config in JVB

--- jitsi-meet/configs/jvb/entrypoint.sh
+++ jitsi-meet/configs/jvb/entrypoint.sh
@@ -4,25 +4,30 @@ set -euo pipefail
 # both jq and curl are needed for shutdown hook
 apt-dpkg-wrap apt-get update && apt-dpkg-wrap apt-get -y install curl jq
 
+
 # JVB baseport can be passed to this script - entrypoint parm can be iterated in deployment spec
 if [[ "$1" =~ ^[0-9]+$ ]]; then
     export JVB_PORT=$1
-    export JVB_TCP_PORT=$2
-    shift
+    shift 
+	if [[ "$1" =~ ^[0-9]+$ ]]; then
+    	export JVB_TCP_PORT=$1
+    	shift
+    else
+    	export JVB_TCP_PORT=$(( $JVB_PORT + 1000 ))
+    fi
 else
-    BASE_PORT=udpstream
+	# ${HOSTNAME##*-} not working with k8s naming
+	HOST_TAIL=${HOSTNAME##*-jvb-}
+	HOST_INDEX=${HOST_TAIL%%-*}
+	[ -z "$BASE_PORT" ] && BASE_PORT=30300
 	# add jvb ID (suffix after - in hostname) to the base port (e.g. 30300 + 1 = 30301)
-	export JVB_PORT=$(($BASE_PORT+${HOSTNAME##*-}))
-	export JVB_TCP_PORT=$(($BASE_PORT+{{ .Values.jvb.tcpoffset }}+${HOSTNAME##*-}))
+	export JVB_PORT=$(($BASE_PORT+$HOST_INDEX))
+	export JVB_TCP_PORT=$(($BASE_PORT+1000+$HOST_INDEX))
 fi
 
 echo "JVB_PORT=$JVB_PORT on server ${HOSTNAME}"
 echo "JVB_TCP_PORT=$JVB_TCP_PORT"
 
 echo "Allowing shutdown of JVB via Rest from localhost..."
-echo "org.jitsi.videobridge.ENABLE_REST_SHUTDOWN=true" >> /defaults/sip-communicator.properties
-echo "org.jitsi.videobridge.shutdown.ALLOWED_SOURCE_REGEXP=127.0.0.1" >> /defaults/sip-communicator.properties
-
-echo "org.ice4j.ice.harvest.DISABLE_AWS_HARVESTER=true" >> /defaults/sip-communicator.properties
 
 exec "$@"
