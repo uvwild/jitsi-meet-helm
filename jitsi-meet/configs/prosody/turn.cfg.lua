5c3cf83fd0fcc530faab6efeb2328d016ddabdb7
UV <uvwild@gmail.com>
2021-02-23 16:34:55 +0000

    fix turnconfig

--- jitsi-meet/configs/prosody/turn.cfg.lua
+++
@@ -1,8 +0,0 @@
--- /defaults/conf.d/turn.cfg.lua
-turncredentials_secret = "{{ .Env.TURN_AUTH_PASSWORD | default 'uebersafe' }}"
-
-turncredentials = {
-  { type = "turn", host = "{{ .Env.TURN_HOST }}", port = "{{ .Env.TURN_PORT }}", transport = "udp" },
-  { type = "turns", host = "{{ .Env.TURN_HOST }}", port = "{{ .Env.TURNS_PORT }}", transport = "tcp" }
-};
-
