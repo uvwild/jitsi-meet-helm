71d422ee607ba7cb1e7ab3e3b3264b200d20b28b
UV <uvwild@gmail.com>
2021-03-09 17:58:39 +0000

    fix dns helper

--- otc/fix-jitsi-dns.sh
+++ otc/fix-jitsi-dns.sh
@@ -2,31 +2,34 @@
 # create DNS entry for jitsi web
 # this is obsoletet because of Istio gateway services
 
+HOSTNAME=$(yq e '.webHost' otc/values.yaml)
 BASE_ZONE=otcdemo.gardener.t-systems.net.
-JITSI_RECORD=jitsi.$BASE_ZONE
-JITSI_IP=$(openstack dns recordset list $BASE_ZONE -f json | jq -r --arg jitsi $JITSI_RECORD '.[] | select (.name==$jitsi).records[0]')
+JITSI_RECORD="${HOSTNAME}."
+JITSI_OLDIP=$(openstack dns recordset list $BASE_ZONE -f json | jq -r --arg jitsi $JITSI_RECORD '.[] | select (.name==$jitsi).records[0]')
 
 
-echo DOMAIN:$JITSI_RECORD-  OLDIP:$JITSI_IP-
+echo DOMAIN:$JITSI_RECORD-  OLDIP:$JITSI_OLDIP-
 
 PENDING="<pending>"
 JITSI_NEWIP=$PENDING
 
+INGRESS=jitsi-meet-ingress-nginx-controller
+
 while [[ "80.158" != "${JITSI_NEWIP:0:6}" ]]
 do
-	JITSI_NEWIP=$(kubectl -n jitsi get svc | grep jitsi-otc-web\  | awk '{print $4}')
+	JITSI_NEWIP=$(kubectl -n jitsi get svc | grep $INGRESS | grep LoadBalancer | awk '{print $4}')
 	echo -n .
 	sleep 2
 done
 echo new loadBalancer IP  $JITSI_NEWIP
 echo -n "Change entry for DOMAIN:${JITSI_RECORD}- "
-echo  "OLDIP:${JITSI_IP}- ==>  -NEWIP:${JITSI_NEWIP}-"
+echo  "OLDIP:${JITSI_OLDIP}- ==>  -NEWIP:${JITSI_NEWIP}-"
 
-if [ "$JITSI_IP" == "$JITSI_NEWIP" ];then
+if [ "$JITSI_OLDIP" == "$JITSI_NEWIP" ];then
 		echo -e "\e[32m NO CHANGE DONE\e[0m"
-else		
-	if [ -n "$JITSI_IP" ] ; then	
-		echo -e "\e[31mRemove previous Entry with  $JITSI_NEWIP\e[0m"
+else
+	if [ -n "$JITSI_OLDIP" ] ; then
+		echo -e "\e[31mRemove previous Entry with  $JITSI_OLDIP\e[0m"
 		echo openstack dns recordset delete $BASE_ZONE $JITSI_RECORD
 		openstack dns recordset delete $BASE_ZONE $JITSI_RECORD
 	fi
@@ -35,7 +38,6 @@ else
 	openstack dns recordset create --type A --name $JITSI_RECORD --record "$JITSI_NEWIP"  $BASE_ZONE
 fi
 
-ping -c 1 $JITSI_IP
-
-
-ping -c 1 ${JITSI_RECORD%%.}
+echo -n OLDIP\ ;ping -c 1 $JITSI_OLDIP
+echo -n NEWIP\ ;ping -c 1 $JITSI_NEWIP
+echo -n HOSTNAME\ ;ping -c 1 ${JITSI_RECORD%%.}
